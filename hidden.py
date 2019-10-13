403fac71beb1815aa17582831207a190d387edce
Aldas <40773946+AXDZ@users.noreply.github.com>
2019-10-13 16:28:08 +0100

    Add files via upload

--- hidden.py
+++ hidden.py
@@ -1,3 +1,5 @@
-token= "discord_bot_token"
+token= "your_discord_bot_token"
 
-theapi_key = "api_key_that_you_get_from_ipdata.co"
+prefix = "your_preferred_prefix"
+
+lapi_key = "your_api_key_from_ipdata.co"
